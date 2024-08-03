ad6a517c244760e598178966a1ed7cd39e22d331
zheng <60832231+Zheng-MJ@users.noreply.github.com>
2024-08-03 19:49:21 +0800

    update

--- basicsr/utils/feature_show.py
+++ basicsr/utils/feature_show.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
 
-def feature_show(x, name):
-    flag = 0
+def feature_show(x, name, save_dir = "plt"):
     x = x.squeeze().cpu().numpy()
     x = np.mean(x,axis=0)
-  #  x = (x-np.min(x))/(np.max(x)-np.min(x))
     
     f = np.fft.ifft2(x)
     fshift = np.fft.fftshift(f)
@@ -18,12 +16,8 @@ def feature_show(x, name):
     plt.xticks([])
     plt.yticks([])
 
-    if flag == 1 :
-      vnorm = mpl.colors.Normalize(vmin=-13,vmax=-3)
-      plt.imshow(x, cmap='jet',norm=vnorm)
-    else:
-      plt.imshow(x , cmap='jet')
+    plt.imshow(x , cmap='jet')
     plt.colorbar()
-    plt.savefig(os.path.join("plt",name), dpi=200)
+    plt.savefig(os.path.join(save_dir, name), dpi=200)
     plt.close()
     
